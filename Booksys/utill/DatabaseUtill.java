155b2470f338eada47833928a80869669b9442b7
SEDTeam <SEDTeam@39.117.95.252>
2022-05-10 23:10:29 +0900

    서버 컴퓨터 커밋입니다!

--- Booksys/utill/DatabaseUtill.java
+++ Booksys/utill/DatabaseUtill.java
@@ -6,10 +6,10 @@ import java.sql.DriverManager;
 public class DatabaseUtill {
 	public static Connection getConnection() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/booksys_db";
+			String dbURL = "jdbc:mysql://localhost:3306/booksys";
 			String dbID = "root";
-			String dbPW = "sedteamd";
-			Class.forName("com.mysql.jdbc.Driver");
+			String dbPW = "software!";
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			return DriverManager.getConnection(dbURL, dbID, dbPW);
 		}catch(Exception e) {
 			e.printStackTrace();
